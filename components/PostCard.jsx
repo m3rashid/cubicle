26af14afae01b327799ba9bdb10f2c2afeb87dd6
m3rashid <mdrashid.hussain.786.01@gmail.com>
2021-12-07 22:14:26 +0530

    idk

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -6,6 +6,13 @@ const PostCard = ({ post }) => (
   <div className="bg-gray-700 shadow-lg rounded-md lg:rounded-lg p-0">
     <div className="relative overflow-hidden pb-80 mb-6">
       <img src={post.featuredImage.url} alt={post.excerpt} className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg" />
+      <div className="mb-2 font-bold text-white absolute bottom-2 right-2">
+        {post.categories.map(cat => {
+          return (
+            <span className="bg-gradient-to-r from-red-500 to-yellow-500 text-gray-900 font-bold rounded-md p-1 mr-1" key={cat.slug}>{cat.name}</span>
+          )
+        })}
+      </div>
     </div>
     <h2 className="text-center mb-4 px-2 lg:px-4 cursor-pointer text-3xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-red-500 to-yellow-500">
       <Link href={`/post/${post.slug}`}>{post.title}</Link>
